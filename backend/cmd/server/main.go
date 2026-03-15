93ac8b31ada0bcaad283ff86b813f075eb0bc58c
aha-hyeong <ahahyeong@gmail.com>
2026-03-15 21:05:30 +0900

    chore: v0.11.1, 버전 업

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -101,7 +101,7 @@ func main() {
 
 	// Fiber 앱 생성
 	app := fiber.New(fiber.Config{
-		AppName:   "Kumiho API v0.11.0",
+		AppName:   "Kumiho API v0.11.1",
 		BodyLimit: 50 * 1024 * 1024, // 50MB
 		ErrorHandler: func(c *fiber.Ctx, err error) error {
 			code := fiber.StatusInternalServerError
