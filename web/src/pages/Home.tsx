2a43ab95f6b15dc217b570f128120276b7be12fd
aha-hyeong <ahahyeong@gmail.com>
2026-04-22 22:23:00 +0900

    fix: navigateTo 테스트 추가 및 불필요한 조건 분기 제거

--- web/src/pages/Home.tsx
+++ web/src/pages/Home.tsx
@@ -319,7 +319,7 @@ export function HomePage() {
                 progress={progress.progress_percent}
                 chapterId={progress.chapter_id}
                 volumeId={progress.volume_id}
-                navigateTo={progress.series_id ? `/series/${progress.series_id}` : undefined}
+                navigateTo={`/series/${progress.series_id}`}
                 onStatusChange={loadData}
                 showExtensionBadge
                 extensionBadgeText={recentProgressExtensionMap[progress.id]}
